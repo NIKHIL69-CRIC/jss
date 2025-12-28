ac5fba42e464b67263ff6fdb89d20f68460f74e9
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2025-12-28 17:29:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+var accountPassword="1234456";
+let accountEmail="nick@google.com";
+let accountState;
+
+accountEmail="niks99@google.com"
+accountPassword="22121";
+
+
+
+
+console.table([ accountId, accountPassword,accountEmail,accountState])
+
