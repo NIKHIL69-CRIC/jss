e714c251a1ffa4dd4f8122724c39d9886dd00f77
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2025-12-28 18:31:51 +0000

    commit

---
+++ 01_basics/03_conversionofoperations.js
@@ -0,0 +1,38 @@
+//let score=33;
+
+//console.log(typeof score);
+//console.log(score)
+
+//let count="33"
+
+//console.log(typeof count);
+//console.log(count)
+
+//let score="33abc"
+
+
+
+//let score=null
+//let valueInNumber=Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let show=undefined
+let valueInNumber=Number(show)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn=1
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+let somenumber=68
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
