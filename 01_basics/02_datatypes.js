af82cf37c4514ec90e6b28f4c183e732513933f5
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2025-12-28 18:07:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";  //all nodejs in newer version
+
+let name="niks"
+let age=21;
+let isLoggedin=false
+
+
+console.log(typeof "niks")
+console.log(typeof age)
+
+console.log(typeof undefined)
+console.log(typeof null)
