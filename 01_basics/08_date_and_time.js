e046d7a8703d0a94f489f3865b53000a7b01d9ff
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2025-12-30 17:47:45 +0000

    date_and_time

---
+++ 01_basics/08_date_and_time.js
@@ -0,0 +1,13 @@
+// let myDate=new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.getTime())
+
+let newDate=new Date()
+
+console.log(newDate)
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
