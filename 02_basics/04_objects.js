4292dee8dcea92ff5fb6518679ffe8b74a7d1717
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2026-01-02 11:52:17 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,44 @@
+const tinderUser={}
+tinderUser.id="112abc"
+tinderUser.name="nun"
+tinderUser.isLoggedIn=false
+
+//console.log(tinderUser)
+
+const regularUser={
+    name: "nikhil",
+    email:"name@gmal.com",
+    fullName:{
+        userfullName:{
+            firstName:"nikhil",
+            lastName:"yadav"
+        }
+
+    }
+
+}
+
+// console.log(regularUser.fullName.userfullName.lastName)
+
+const obj1={1:"a" ,2:"b"}
+const obj2={3:"c", 4:"d"}
+const obj3={5:"e", 6:"f"}
+
+// const obj4=Object.assign({}, obj1, obj2, obj3)// to merge the object
+// console.log(obj4)
+
+const obj4={...obj1,...obj2,...obj3}// spread method .
+console.log(obj4)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+// if we want to access keys and values it will show in array
+
+// array of objects
+
+const user=[
+    {
+        id:"1122abc",
+        email:"email@agmial.com",
+    }
+]
