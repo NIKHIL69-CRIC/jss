b7710d1da61c89b1a44387498603383fd8947e5b
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2026-01-13 16:53:41 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -39,11 +39,11 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("hitesh"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+ console.log(calculateCartPrice(200, 400, 500, 2000))
 
 const user = {
     username: "hitesh",
