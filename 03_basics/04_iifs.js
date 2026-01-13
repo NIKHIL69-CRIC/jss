ea91bbcbbc54e11a3111c0b0a147a692f3d8beba
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2026-01-13 18:36:14 +0000

    iifs

---
+++ 03_basics/04_iifs.js
@@ -0,0 +1,7 @@
+(function chai(){
+    console.log("DB is connected")
+})(); // always use semicolon after thhis so we got to know here the function ends
+
+(()=> {
+    console.log("DB is connected")
+})()
