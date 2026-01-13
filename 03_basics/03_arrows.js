2c235c8a4f0a13a1671a77ecc27ed2b9192bfe9f
NIKHIL69-CRIC <nikhilyadav.1629@gmail.com>
2026-01-13 17:50:38 +0000

    arrows

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -20,6 +20,39 @@ user.welcomeMessage()
 function chai(){
     let username="nick"
     console.log(this.username)
+    // ****  whenever we run this under the function it will always show undefined.
 
 }
-chai()
+// chai()
+
+
+ const chai = function () {
+ let username = "hitesh"
+    console.log(this.username);
+}
+
+const chai =  () => {// arrow function..
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+const addTwo = (num1, num2) => {// explicit
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2  // implicit 
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
